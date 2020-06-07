59a01cd81c540ee7e34d19de8c83a0ade07eb985
alew <ale.w.es@sync.money>
2020-06-07 18:01:36 +0200

    initial commit

---
+++ truffle-config.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "265e73b55a2f48e388d1ae298e3488e6";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    develop: {
+      port: 7545
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.6.9",
+    }
+  }
+};
